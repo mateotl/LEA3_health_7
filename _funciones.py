8e6bc830f4bfad430ee4e7e428d04826803c5e94
mateotl <mateo.toro1@udea.edu.co>
2025-06-09 02:25:47 -0500

    despliegue

--- _funciones.py
+++ _funciones.py
@@ -1,34 +1,81 @@
 
 import numpy as np
-
+import pandas as pd
 from os import listdir ### para hacer lista de archivos en una ruta
 from tqdm import tqdm  ### para crear contador en un for para ver evolución
 from os.path import join ### para unir ruta con archivo 
 import cv2 ### para leer imagenes jpg
+from glob import glob
+import zipfile
+import os
+from PIL import Image
+from sklearn.preprocessing import MinMaxScaler
+
+def crear_diccionario(extract_to):
+    imageid_path_dict = {
+        os.path.splitext(os.path.basename(x))[0]: x
+        for x in glob(os.path.join(extract_to, '*.jpg'))
+    }
+    return imageid_path_dict
+
+def cargar_zip(Zip1,Zip2,ExtraerDonde):
+    zip_path1 = Zip1 #'/content/drive/MyDrive/cod/LEA3_health_7/data/HAM10000_images_part_1.zip'
+    zip_path2 = Zip2#'/content/drive/MyDrive/cod/LEA3_health_7/data/HAM10000_images_part_2.zip'
+    extract_to = ExtraerDonde #'/content/ham10000_images'
+
+    os.makedirs(extract_to, exist_ok=True)
+
+    with zipfile.ZipFile(zip_path1, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+
+    with zipfile.ZipFile(zip_path2, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+
+    return extract_to
 
 
-def img2data(path, width=100):
-    
-    rawImgs = []   #### una lista con el array que representa cada imágen
-    labels = [] ### el label de cada imágen
-    
-    list_labels = [path+f for f in listdir(path)] ### crea una lista de los archivos en la ruta (Benigno /Maligno)
+def cargar_csv(CSV):
+    csv_path = CSV #'/content/drive/MyDrive/cod/LEA3_health_7/data/HAM10000_metadata.csv'
+    skin_df = pd.read_csv(csv_path)
+    return skin_df
 
-    for imagePath in list_labels: ### recorre cada carpeta de la ruta ingresada
-        
-        files_list=listdir(imagePath) ### crea una lista con todos los archivos
-        for item in tqdm(files_list): ### le pone contador a la lista: tqdm
-            file = join(imagePath, item) ## crea ruta del archivo
-            if file[-1] =='g': ### verificar que se imágen extensión jpg o jpeg
-                img = cv2.imread(file) ### cargar archivo
-                img = cv2.cvtColor(img , cv2.COLOR_BGR2RGB) ### invierte el orden de los colores en el array para usar el más estándar RGB
-                img = cv2.resize(img ,(width,width)) ### cambia resolución de imágnenes
-                rawImgs.append(img) ###adiciona imágen al array final
-                l = imagePath.split('/')[-1] ### identificar en qué carpeta está
-                if l == 'benign':  ### verificar en qué carpeta está para asignar el label
-                    labels.append([0])
-                elif l == 'malignant':
-                    labels.append([1])
-    return rawImgs, labels, files_list
+# Para preprocesar
+def crear_ruta(df, imageid_path_dict, lesion_type_dict):
+    df = df.copy()
+    df['path'] = df['image_id'].map(imageid_path_dict.get)
+    df['cell_type'] = df['dx'].map(lesion_type_dict.get)
+    return df
 
+def crear_binaria(df):
+    df = df.copy()
+    malignas = ['Actinic keratoses', 'Basal cell carcinoma', 'Melanoma']
+    df['cell_type_idx'] = df['cell_type'].apply(lambda x: 1 if x in malignas else 0)
+    return df
 
+def reemplazar_nulos(df, columna):
+    df = df.copy()
+    df[columna] = df[columna].fillna(df[columna].mean())
+    return df
+
+def redimensionar_imagen(df, size):
+    df = df.copy()
+    df['image'] = df['path'].map(lambda x: np.asarray(Image.open(x).resize(size)))
+    return df
+
+# Para preprocesar datos y entrenar modelos no CNN
+def procesar_tabulares(df):
+    df_tab = df[['age', 'sex', 'localization']].copy()
+    df_tab['age'] = MinMaxScaler().fit_transform(df_tab[['age']])
+    df_tab = pd.get_dummies(df_tab, columns=['sex', 'localization'], drop_first=True)
+    return df_tab
+
+def aplanar_imagenes(df):
+    X_img = np.stack(df['image'].values)                  # (n, 100, 75, 3)
+    X_img_flat = X_img.reshape(X_img.shape[0], -1)        # (n, 22500)
+    return X_img_flat
+
+def combinar_datos(df):
+    X_tab = procesar_tabulares(df)
+    X_img_flat = aplanar_imagenes(df)
+    X_final = np.concatenate([X_img_flat, X_tab.values], axis=1)
+    return X_final
